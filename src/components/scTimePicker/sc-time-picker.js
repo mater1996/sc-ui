e55a4f6203ee75f67039d48f3beb09862e6d1264
xhb <xb864083144@outlook.com>
2018-08-03 08:49:49 +0800

    修复css前缀不一致

--- src/components/scTimePicker/sc-time-picker.js
+++ src/components/scTimePicker/sc-time-picker.js
@@ -26,49 +26,66 @@ Component({
 
     },
     methods: {
-        touchmove(e) {
-            console.log(e);
-        },
         hourtouchmove(e) {
-            if (this.data.px) {
-                let touch = e.touches[0];
-                let {clientX, clientY} = touch;
-                clientX -= e.target.offsetLeft -this.data.timePx ;
-                clientY -= e.target.offsetTop-this.data.timePy;
-                // console.log('hhhhhh', e);
-                let angle = parseInt(this.getAngle(this.data.px, this.data.py, clientX, clientY));
-                // console.log(angle);
-                if ((angle - 15) % 30 === 0) {
-                    angle = angle - 15;
-                    this.setData({
-                        selectTimeRotate: angle
-                    })
-                }
-            } else {
-                this._queryMultipleNodes('.time-hour').then(res => {
-                    this.data.timePx = res[0].left + 130;
-                    this.data.timePy = res[0].top + 130;
-                    // console.log(res);
-                    this.setData({
-                        top: this.data.timePy,
-                        left: this.data.timePx
-                    })
-                    this._queryMultipleNodes('.time-hour-item-select .time-hour-item-number').then(res => {
-                        this.data.px = res[0].left + 16 -this.data.timePx ;
-                        this.data.py = res[0].top + 16 -this.data.timePy;
-                        let touch = e.touches[0];
-                        let {clientX, clientY} = touch;
-                        // console.log(touch);
-                        // console.log('hhhhhh',e);
-                        let angle = this.getAngle(this.data.px, this.data.py, clientX, clientY);
-                        // console.log(angle);
-                        this.setData({
-                            selectTimeRotate: angle
-                        })
-                    })
+            this._queryMultipleNodes('.time-hour').then(res => {
+                // console.log(res);
+                this.setData({
+                    top:res[0].top +(res[0].width/2),
+                    left:res[0].left+(res[0].width)
+                })
+                this.setData({
+                    ctop:res[0].top +(res[0].width/2),
+                    cleft:res[0].left+(res[0].width/2)
+                })
+                // console.log(this.data.top,this.data.left);
+                this.setData({
+                    ptop:e.changedTouches[0].clientY,
+                    pleft:e.changedTouches[0].clientX
                 });
-
-            }
+                let angle = this.getAngle(this.data.pleft-this.data.cleft,this.data.ptop-this.data.ctop,this.data.left-this.data.cleft,this.data.top-this.data.ctop);
+                console.log(angle);
+                    // this.setData({
+                    //                 selectTimeRotate: angle
+                    //             })
+            });
+            // if (this.data.px) {
+            //     let touch = e.touches[0];
+            //     let {clientX, clientY} = touch;
+            //     clientX -= e.target.offsetLeft -this.data.timePx ;
+            //     clientY -= e.target.offsetTop-this.data.timePy;
+            //     // console.log('hhhhhh', e);
+            //     let angle = parseInt(this.getAngle(this.data.px, this.data.py, clientX, clientY));
+            //     // console.log(angle);
+            //     if ((angle - 15) % 30 === 0) {
+            //         angle = angle - 15;
+            //         this.setData({
+            //             selectTimeRotate: angle
+            //         })
+            //     }
+            // } else {
+            //     this._queryMultipleNodes('.time-hour').then(res => {
+            //         this.data.timePx = res[0].left + 130;
+            //         this.data.timePy = res[0].top + 130;
+            //         this.setData({
+            //             top: this.data.timePy,
+            //             left: this.data.timePx
+            //         })
+            //         this._queryMultipleNodes('.time-hour-item-select .time-hour-item-number').then(res => {
+            //             this.data.px = res[0].left + 16 -this.data.timePx ;
+            //             this.data.py = res[0].top + 16 -this.data.timePy;
+            //             let touch = e.touches[0];
+            //             let {clientX, clientY} = touch;
+            //             // console.log(touch);
+            //             // console.log('hhhhhh',e);
+            //             let angle = this.getAngle(this.data.px, this.data.py, clientX, clientY);
+            //             // console.log(angle);
+            //             this.setData({
+            //                 selectTimeRotate: angle
+            //             })
+            //         })
+            //     });
+            //
+            // }
         },
         _queryMultipleNodes: function (e) {
             return new Promise((resolve, reject) => {
@@ -80,10 +97,11 @@ Component({
                 });
             })
         },
-        getAngle(px, py, mx, my) {    // 起始点  结束点
-            console.log('起始点',px,py);
-            console.log('结束点',mx,my);
-            return Math.atan2(my - py, mx - px) / Math.PI * 180
+        getAngle(x1, y1, x2, y2) {    // 起始点  结束点
+            return this.angleTwoPoints(x1, y1, x2, y2) / Math.PI * 180 *2
+        },
+        angleTwoPoints() {
+            return Math.atan2(arguments[3] - arguments[1], arguments[2] - arguments[0])
         }
     }
 });
